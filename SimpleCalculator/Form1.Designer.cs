40021a3b96525a6ddc7a312c22005d67c13fb0d1
ajshoeppner <50330118+ajshoeppner@users.noreply.github.com>
2019-11-06 23:39:17 -0600

    Add files via upload

---
+++ SimpleCalculator/Form1.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace SimpleCalculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Addition = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Division = new System.Windows.Forms.Button();
+            this.Multiplication = new System.Windows.Forms.Button();
+            this.Subtraction = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Addition
+            // 
+            this.Addition.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.Addition.Location = new System.Drawing.Point(357, 123);
+            this.Addition.Name = "Addition";
+            this.Addition.Size = new System.Drawing.Size(35, 35);
+            this.Addition.TabIndex = 4;
+            this.Addition.Text = "+";
+            this.Addition.UseVisualStyleBackColor = true;
+            this.Addition.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(357, 97);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(163, 20);
+            this.textBox2.TabIndex = 6;
+            this.textBox2.TextChanged += new System.EventHandler(this.TextBox2_TextChanged_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(354, 81);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 7;
+            // 
+            // Division
+            // 
+            this.Division.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.Division.Location = new System.Drawing.Point(485, 123);
+            this.Division.Name = "Division";
+            this.Division.Size = new System.Drawing.Size(35, 35);
+            this.Division.TabIndex = 8;
+            this.Division.Text = "/";
+            this.Division.UseVisualStyleBackColor = true;
+            this.Division.Click += new System.EventHandler(this.Button1_Click_1);
+            // 
+            // Multiplication
+            // 
+            this.Multiplication.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.Multiplication.Location = new System.Drawing.Point(485, 164);
+            this.Multiplication.Name = "Multiplication";
+            this.Multiplication.Size = new System.Drawing.Size(35, 35);
+            this.Multiplication.TabIndex = 9;
+            this.Multiplication.Text = "x";
+            this.Multiplication.UseVisualStyleBackColor = true;
+            this.Multiplication.Click += new System.EventHandler(this.Multiplication_Click_1);
+            // 
+            // Subtraction
+            // 
+            this.Subtraction.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.Subtraction.Location = new System.Drawing.Point(357, 164);
+            this.Subtraction.Name = "Subtraction";
+            this.Subtraction.Size = new System.Drawing.Size(35, 35);
+            this.Subtraction.TabIndex = 10;
+            this.Subtraction.Text = "-";
+            this.Subtraction.UseVisualStyleBackColor = true;
+            this.Subtraction.Click += new System.EventHandler(this.Subtraction_Click_1);
+            // 
+            // button1
+            // 
+            this.button1.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.button1.Location = new System.Drawing.Point(398, 205);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(35, 35);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "=";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click_2);
+            // 
+            // button2
+            // 
+            this.button2.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.button2.Location = new System.Drawing.Point(444, 205);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(35, 35);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "CE";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.button3.Location = new System.Drawing.Point(398, 123);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(81, 35);
+            this.button3.TabIndex = 13;
+            this.button3.Text = "To Binary";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.button4.Location = new System.Drawing.Point(398, 164);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(81, 35);
+            this.button4.TabIndex = 14;
+            this.button4.Text = "From Binary";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.Button4_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(368, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "label2";
+            this.label2.Click += new System.EventHandler(this.Label2_Click);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.Addition;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Thistle;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Subtraction);
+            this.Controls.Add(this.Multiplication);
+            this.Controls.Add(this.Division);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.Addition);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button Addition;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button Division;
+        private System.Windows.Forms.Button Multiplication;
+        private System.Windows.Forms.Button Subtraction;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ SimpleCalculator/Form1.cs
@@ -0,0 +1,277 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SimpleCalculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+
+            if (calculator.Mode == null)
+            {
+                calculator.Mode = "Add";
+            }
+            else
+            {
+                calculator.Answer(int.Parse(textBox2.Text));
+                calculator.Mode = "Add";
+                label1.Text = calculator.Log;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void TextBox2_TextChanged(object sender, EventArgs e)
+        {
+            if (calculator.AnswerNumber == null)
+            {
+                calculator.AnswerNumber = int.Parse(textBox2.Text);
+            }
+        }
+        private void Addition_Click(object sender, EventArgs e)
+        {
+
+            if (calculator.Mode == null)
+            {
+                calculator.Mode = "Add";
+            }
+            else
+            {
+                calculator.Answer(int.Parse(textBox2.Text));
+                calculator.Mode = "Add";
+                label1.Text = calculator.Log;
+            }
+        }
+        private void Subtraction_Click(object sender, EventArgs e)
+        {
+
+            if (calculator.Mode == null)
+            {
+                calculator.Mode = "Subtract";
+            }
+            else
+            {
+                calculator.Answer(int.Parse(textBox2.Text));
+                calculator.Mode = "Subtract";
+                label1.Text = calculator.Log;
+            }
+        }
+        private void Multiplication_Click(object sender, EventArgs e)
+        {
+
+            if (calculator.Mode == null)
+            {
+                calculator.Mode = "Multiply";
+            }
+            else
+            {
+                calculator.Answer(int.Parse(textBox2.Text));
+                calculator.Mode = "Multiply";
+                label1.Text = calculator.Log;
+            }
+
+        }
+        private void Division_Click(object sender, EventArgs e)
+        {
+
+            if (calculator.Mode == null)
+            {
+                calculator.Mode = "Divide";
+            }
+            else
+            {
+                calculator.Answer(int.Parse(textBox2.Text));
+                calculator.Mode = "Divide";
+                label1.Text = calculator.Log;
+            }
+        }
+
+        static Calculator calculator = new Calculator();
+        public class Calculator
+        {
+            public string Log { get; set; }
+            public int? AnswerNumber { get; set; }
+            public string Mode { get; set; }
+            public string Addition(int n)
+            {
+                AnswerNumber = AnswerNumber + n;
+                string result = "" + AnswerNumber;
+                Log = Log + "+" + n;
+                return result;
+            }
+            public string Subtraction(int n)
+            {
+                AnswerNumber = AnswerNumber - n;
+                string result = "" + AnswerNumber;
+                Log = Log + "-" + n;
+                return result;
+            }
+            public string Multiplication(int n)
+            {
+                AnswerNumber = AnswerNumber * n;
+                string result = "" + AnswerNumber;
+                Log = Log + "x" + n;
+                return result;
+            }
+            public string Division(int n)
+            {
+                AnswerNumber = AnswerNumber / n;
+                string result = "" + AnswerNumber;
+                Log = Log + "/" + n;
+                return result;
+            }
+            public string MakeBinary(int n)
+            {
+                string result = "";
+                while (n > 0)
+                {
+                    int reminder = n % 2;
+                    result = reminder + result;
+                    n = n / 2;
+                }
+                return result;
+            }
+            public string FromBinary(int n)
+            {
+                int decimalVal = 0;
+                int baseVal = 1;
+                int rem;
+                string result = "";
+                while (n > 0)
+                {
+                    rem = n % 10;
+                    decimalVal = decimalVal + rem * baseVal;
+                    n = n / 10;
+                    baseVal = baseVal * 2;
+                    result = result + decimalVal;
+                }
+                return result;
+            }
+            public void Answer(int n)
+            {
+                if (Mode == "Add")
+                {
+                    Addition(n);
+                }
+                else if (Mode == "Subtract")
+                {
+                    Subtraction(n);
+                }
+                else if (Mode == "Multiply")
+                {
+                    Multiplication(n);
+                }
+                else if (Mode == "Divide")
+                {
+                    Division(n);
+                }
+                else
+                {
+                    Log = Log + AnswerNumber;
+                }
+            }
+        }
+
+        private void Button1_Click_1(object sender, EventArgs e)
+        {
+
+            if (calculator.Mode == null)
+            {
+                calculator.Mode = "Add";
+            }
+            else
+            {
+                calculator.Answer(int.Parse(textBox2.Text));
+                calculator.Mode = "Add";
+                label1.Text = calculator.Log;
+            }
+        }
+
+        private void Label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Subtraction_Click_1(object sender, EventArgs e)
+        {
+            if (calculator.Mode == null)
+            {
+                calculator.Mode = "Subtract";
+            }
+            else
+            {
+                calculator.Answer(int.Parse(textBox2.Text));
+                calculator.Mode = "Subtract";
+                label1.Text = calculator.Log;
+            }
+        }
+
+        private void TextBox2_TextChanged_1(object sender, EventArgs e)
+        {
+            if (calculator.AnswerNumber == null)
+            {
+                calculator.AnswerNumber = int.Parse(textBox2.Text);
+            }
+        }
+
+        private void Multiplication_Click_1(object sender, EventArgs e)
+        {
+            if (calculator.Mode == null)
+            {
+                calculator.Mode = "Multiply";
+            }
+            else
+            {
+                calculator.Answer(int.Parse(textBox2.Text));
+                calculator.Mode = "Multiply";
+                label1.Text = calculator.Log;
+            }
+        }
+
+        private void Button4_Click(object sender, EventArgs e)
+        {
+            label2.Text = calculator.FromBinary(int.Parse(textBox2.Text));
+
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            label2.Text = calculator.MakeBinary(int.Parse(textBox2.Text));
+        }
+
+        private void Button1_Click_2(object sender, EventArgs e)
+        {
+            calculator.Answer(int.Parse(textBox2.Text));
+            label1.Text = calculator.Log + calculator.AnswerNumber;
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            calculator.Mode = null;
+            calculator.AnswerNumber = null;
+            calculator.Log = null;
+            label1.Text = calculator.Log;
+            label2.Text = null;
+        }
+    }
+}
